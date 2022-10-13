0202cc0d2579438743f80df75bd418a6c79baac4
guot54 <guot55@163.com>
2022-10-13 08:16:44 +0000

    更新 'drpy.js'

--- drpy.js
+++ drpy.js
@@ -1,4 +1,4 @@
-import './lib/util.js';
+import 'https://agit.ai/guot54/ygbh/raw/branch/master/lib/util.js';
 import cheerio from 'assets://js/lib/cheerio.min.js';
 import 'assets://js/lib/crypto-js.js'
 // import muban from 'https://gitcode.net/qq_32394351/dr_py/-/raw/master/js/模板.js';
